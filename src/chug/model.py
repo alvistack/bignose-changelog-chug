c0b74fb45eacd871ed030d5e9a9513e91f488a0e
Ben Finney <ben+python@benfinney.id.au>
2024-10-10 13:20:24 +1100

    Implement a regular expression to parse an RFC822-style person value.

---
+++ src/chug/model.py
@@ -0,0 +1,29 @@
+# src/chug/model.py
+# Part of ‘changelog-chug’, a parser for project Change Log documents.
+#
+# This is free software, and you are welcome to redistribute it under
+# certain conditions; see the end of this file for copyright
+# information, grant of license, and disclaimer of warranty.
+
+""" Data model for internal representation. """
+
+import re
+
+
+rfc822_person_regex = re.compile(r"^(?P<name>[^<]+) <(?P<email>[^>]+)>$")
+""" Regular Expression pattern to match a person's contact details. """
+
+
+# Copyright © 2008–2024 Ben Finney <ben+python@benfinney.id.au>
+#
+# This is free software: you may copy, modify, and/or distribute this work
+# under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation; version 3 or, at your option, a later version.
+# No warranty expressed or implied. See the file ‘LICENSE.AGPL-3’ for details.
+
+
+# Local variables:
+# coding: utf-8
+# mode: python
+# End:
+# vim: fileencoding=utf-8 filetype=python :
